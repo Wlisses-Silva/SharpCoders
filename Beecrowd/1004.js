6b4d935d2abb6154f8f382ab30686bf56d9b5064
Wlisses Silva <wlisses.silva@outlook.com>
2023-10-19 14:38:11 -0300

    Problema do Beecrowd 1004 - Produto Simples

---
+++ Beecrowd/1004.js
@@ -0,0 +1,12 @@
+/*------------------ 1004 - Produto Simples ------------------*/
+//Variáveis
+var v1 = 3;
+var v2 = 9;
+// var A = parseInt(lines.shift());
+// var B = parseInt(lines.shift());
+
+// Cálculo
+var PROD = v1 * v2;
+
+// Retorno
+console.log("PROD = " + PROD);
