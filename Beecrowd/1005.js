72e95e24679865b5e987a87500626a4a2df80828
Wlisses Silva <wlisses.silva@outlook.com>
2023-10-19 14:56:06 -0300

    Problema do Beecrowd 1005 - Media 1

---
+++ Beecrowd/1005.js
@@ -0,0 +1,12 @@
+/*------------------ 1005 - Media 1 ------------------*/
+//Variáveis
+var A = 5.0;
+var B = 7.1;
+// var A = parseFloat(lines.shift());
+// var B = parseFloat(lines.shift());
+
+// Cálculo
+var MEDIA = (A * 3.5 + B * 7.5) / 11
+
+// Retorno
+console.log("MEDIA = " + MEDIA.toFixed(5));
