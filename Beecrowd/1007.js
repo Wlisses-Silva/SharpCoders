27de5f27af7019eeec2e3e1940a2caa284cf36db
Wlisses Silva <wlisses.silva@outlook.com>
2023-10-19 15:32:35 -0300

    Problema do Beecrowd 1007 - Diferença

---
+++ Beecrowd/1007.js
@@ -0,0 +1,17 @@
+/*------------------ 1007 - Diferença ------------------*/
+//Variáveis
+// let A = 5
+// let B = 6
+// let C = 7 
+// let D = 8
+
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
+var C = parseInt(lines.shift());
+var D = parseInt(lines.shift());
+
+// Cálculo
+var  DIFERENCA = (A * B - C * D)
+
+// Retorno
+console.log("DIFERENCA = " + DIFERENCA);
