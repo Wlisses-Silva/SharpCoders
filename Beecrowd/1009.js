54a8826e806a37726d23de5d12255056c0610ce7
Wlisses Silva <wlisses.silva@outlook.com>
2023-10-19 16:50:39 -0300

    Problema do Beecrowd 1009 - Salário com Bônus

---
+++ Beecrowd/1009.js
@@ -0,0 +1,16 @@
+/*------------------ 1009 - Salário com Bônus------------------*/
+// //Variáveis
+let nome = "Joao"
+let salarioFixo = 500.00
+let valorDeVendas = 1230.30
+
+// let nome = lines.shift()
+// let salarioFixo = parseFloat(lines.shift())
+// let valorDeVendas = parseFloat(lines.shift())
+
+// Cálculo
+let comissao = valorDeVendas * 0.15
+let salarioTotal = salarioFixo + comissao
+
+// Retorno
+console.log("TOTAL = R$ " + salarioTotal.toFixed(2))
