9037263281080985ff7cec88252094e98f97b7ac
Wlisses Silva <wlisses.silva@outlook.com>
2023-10-19 10:25:28 -0300

    Exercício 1001 do Beecrowd

---
+++ Beecrowd/1001.js
@@ -0,0 +1,18 @@
+//Normalmente se faria assim
+
+// var A = 10
+// var B = 9 
+
+// X = A + B 
+// console.log(X)
+
+//Mas, no Beecrowd é assim: 
+// Variáveis
+let A = parseInt(lines.shift());
+let B = parseInt(lines.shift());
+
+// Cálculo
+let X = A+B;
+
+// Retorno
+console.log("X = "+X);
