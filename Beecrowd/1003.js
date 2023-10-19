2e8bbd254400b96e7db9f2b18edf2cf7f07a4857
Wlisses Silva <wlisses.silva@outlook.com>
2023-10-19 14:19:32 -0300

    Problema do Beecrowd 1003 - Soma Simples

---
+++ Beecrowd/1003.js
@@ -0,0 +1,12 @@
+/*------------------ 1003 - Simples Soma ------------------*/
+//Variáveis
+var A = 30
+var B = 10
+// var A = parseInt(lines.shift());
+// var B = parseInt(lines.shift());
+
+// Cálculo
+var SOMA = A + B; 
+
+// Retorno
+console.log("SOMA = " + SOMA);
