a1531210f1604dfd427c386f4b525f4ca699fcb9
Wlisses Silva <wlisses.silva@outlook.com>
2023-10-19 15:10:11 -0300

    Problema do Beecrowd 1006 - Media 2

---
+++ Beecrowd/1006.js
@@ -0,0 +1,14 @@
+/*------------------ 1006 - Media 2 ------------------*/
+//Variáveis
+var A = 5.0;
+var B = 6.0;
+var C = 7.0
+// var A = parseFloat(lines.shift());
+// var B = parseFloat(lines.shift());
+// var B = parseFloat(lines.shift());
+
+// Cálculo
+var MEDIA = (A * 2 + B * 3 + C * 5) / 10
+
+// Retorno
+console.log("MEDIA = " + MEDIA.toFixed(1));
