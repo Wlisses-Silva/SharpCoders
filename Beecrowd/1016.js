b829d834caf255cdd0353020694026c92747f8ff
Wlisses Silva <wlisses.silva@outlook.com>
2023-10-20 14:47:43 -0300

    Problema do Beecrowd 1016 - Distancia

---
+++ Beecrowd/1016.js
@@ -0,0 +1,10 @@
+/*------------------ 1016 - Distância ------------------*/
+//Variáveis
+let distancia = 30
+//let distancia = parseInt(lines.shift())
+
+// Cálculo
+let tempoDistancia = distancia * 2
+
+// Retorno
+console.log(tempoDistancia + " Minutos")
