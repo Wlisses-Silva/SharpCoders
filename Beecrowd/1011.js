3e7166a8b99a55a57e90bedb59d8e1085143eff4
Wlisses Silva <wlisses.silva@outlook.com>
2023-10-20 09:34:57 -0300

    Problema do Beecrowd 1011 - Esfera

---
+++ Beecrowd/1011.js
@@ -0,0 +1,11 @@
+/*------------------ 1011 - Esfera------------------*/
+//Variáveis
+let pi = 3.14159
+let raio = 3
+//let raio = parseFloat(lines.shift())
+
+// Cálculo
+let volume = (4/3) * pi * Math.pow(raio, 3)
+
+// Retorno
+console.log("VOLUME = " + volume.toFixed(3))
