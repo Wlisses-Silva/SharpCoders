13294f49ae249ba65a413001b10b7a10fefc52ee
Wlisses Silva <wlisses.silva@outlook.com>
2023-10-20 11:49:05 -0300

    Problema do Beecrowd 1013 - O Maior

---
+++ Beecrowd/1013.js
@@ -0,0 +1,12 @@
+/*------------------ 1013 - O Maior ------------------*/
+//Variáveis
+let A = 7
+let B = 14
+let C = 106
+let [A, B, C] = input.split(" ").map((item) => parseInt(item) )
+// Cálculo
+let AB = ((A+B) + Math.abs((A-B))) / 2
+let maiorValor = ((AB + C) + Math.abs((AB - C))) / 2
+
+// Retorno
+console.log(`${maiorValor} eh o maior`) 
