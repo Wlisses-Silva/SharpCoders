a4fdb8f79ab39ef2a703dae1ab82f650ec64ea54
Wlisses Silva <wlisses.silva@outlook.com>
2023-10-26 09:03:01 -0300

    Atualizando as atividaes 1021 e 1059 do Beecrowd

---
+++ Beecrowd/1059.js
@@ -0,0 +1,14 @@
+/*------------------ 1059 - Números Positivos ---------------*/
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let valor = 0; 
+
+for (let i = 0; i < 6; i++) {
+    let numero = parseFloat(lines.shift());
+    if (numero > 0) {
+        valor++
+    }
+}
+console.log(valor + " valores positivos")
